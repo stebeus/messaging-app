cd20bcb0208d421713bfd90ec90170d11041d244
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-26 14:49:16 -0300

    feat(api): route conversations

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from 'hono';
 
+import { conversations } from './conversations/index.ts';
 import { users } from './users/index.ts';
 
 export const routes = new Hono();
 
+routes.route('/conversations', conversations);
 routes.route('/users', users);
