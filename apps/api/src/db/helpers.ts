9a1ddfd3f47d7cda0d63bc4596dd14e6422de028
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-27 11:49:23 -0300

    refactor(api): return many records

--- apps/api/src/db/helpers.ts
+++ apps/api/src/db/helpers.ts
@@ -3,10 +3,8 @@ import type { PgInsertValue, PgTable, PgUpdateSetSource } from 'drizzle-orm/pg-c
 
 import { db } from './client.ts';
 
-export const insert = async <Table extends PgTable>(table: Table, values: PgInsertValue<Table>) => {
-	const [data] = await db.insert(table).values(values).returning();
-	return data;
-};
+export const insert = async <Table extends PgTable>(table: Table, values: PgInsertValue<Table>) =>
+	await db.insert(table).values(values).returning();
 
 export const update = async <Table extends PgTable>(
 	table: Table,
@@ -14,7 +12,5 @@ export const update = async <Table extends PgTable>(
 	condition: SQL,
 ) => await db.update(table).set(values).where(condition).returning();
 
-export const del = async <Table extends PgTable>(table: Table, condition?: SQL) => {
-	const [data] = await db.delete(table).where(condition).returning();
-	return data;
-};
+export const del = async <Table extends PgTable>(table: Table, condition?: SQL) =>
+	await db.delete(table).where(condition).returning();
