9df84245b0ea40505af55a2bcbd8a92b4ff95d6f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-28 07:06:27 -0300

    feat(contracts): add `PatchUserBody` dto

--- packages/contracts/src/users/dto.ts
+++ packages/contracts/src/users/dto.ts
@@ -2,8 +2,14 @@ import z from 'zod';
 
 import { id } from '#utils.js';
 
+import { NewUser } from './entity.js';
+
+export const PatchUserBody = NewUser.partial();
+
 export const UserParams = z.object({
 	userId: id,
 });
+
+export type PatchUserBody = z.infer<typeof PatchUserBody>;
 
 export type UserParams = z.infer<typeof UserParams>;
