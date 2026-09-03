42ec86f6f681f95cb961c77a2415273bc9506e83
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-03 07:00:19 -0300

    feat(api): add utils

---
+++ apps/api/src/utils/errors.ts
@@ -0,0 +1,41 @@
+import type { ContentfulStatusCode } from 'hono/utils/http-status';
+
+import { STATUS_CODES } from 'node:http';
+
+import { HTTPException } from 'hono/http-exception';
+
+type HttpErrorOptions = Partial<{
+	res: Response;
+	message: string;
+	cause: unknown;
+}>;
+
+export class HttpError extends HTTPException {
+	static isHttpError(value: unknown) {
+		return value instanceof HTTPException;
+	}
+
+	readonly message;
+	readonly cause;
+
+	constructor(
+		status: ContentfulStatusCode = 500,
+		{ res, message = STATUS_CODES[status], cause }: HttpErrorOptions = {},
+	) {
+		super(status, { res, message, cause });
+		this.message = message ?? 'Internal Server Error';
+		this.cause = cause;
+	}
+}
+
+export class ForbiddenError extends HttpError {
+	constructor(options?: HttpErrorOptions) {
+		super(403, options);
+	}
+}
+
+export class NotFoundError extends HttpError {
+	constructor(options?: HttpErrorOptions) {
+		super(404, options);
+	}
+}
