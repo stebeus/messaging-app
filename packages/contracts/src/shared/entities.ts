425e22156b3ca5b7e095c5035e305ae600a786cf
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-03 06:45:59 -0300

    refactor(contracts): rename fields to entities

---
+++ packages/contracts/src/shared/entities.ts
@@ -0,0 +1,31 @@
+import * as z from 'zod';
+
+export const id = z.coerce.number().int().positive();
+
+export const createdAt = z.date();
+
+export const updatedAt = z.date();
+
+export const timestamps = { createdAt: true, updatedAt: true } as const;
+
+export const base = { ...timestamps, id: true } as const;
+
+export const Id = z.object({
+	id,
+});
+
+export const Timestamps = z.object({
+	createdAt,
+	updatedAt,
+});
+
+export const Base = z.object({
+	...Id.shape,
+	...Timestamps.shape,
+});
+
+export type Id = z.infer<typeof Id>;
+
+export type Base = z.infer<typeof Base>;
+
+export type Timestamps = z.infer<typeof Timestamps>;
