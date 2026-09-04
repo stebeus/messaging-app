5d49ab8f00c0bb157aae6171bf770d6aac577f7c
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-04 13:22:41 -0300

    feat(contracts): add friend request dtos

--- packages/contracts/src/modules/friendships/dto.ts
+++ packages/contracts/src/modules/friendships/dto.ts
@@ -6,4 +6,16 @@ export const FriendParameters = z.object({
 	friendId: id,
 });
 
+export const SendFriendRequestParameters = z.object({
+	recipientId: id,
+});
+
+export const AcceptFriendRequestParameters = z.object({
+	senderId: id,
+});
+
 export type FriendParameters = z.infer<typeof FriendParameters>;
+
+export type SendFriendRequestParameters = z.infer<typeof SendFriendRequestParameters>;
+
+export type AcceptFriendRequestParameters = z.infer<typeof AcceptFriendRequestParameters>;
