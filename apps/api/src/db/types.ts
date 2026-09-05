b4a4351f396767922aa52f3b727ee93dc6627ea1
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-05 13:46:58 -0300

    feat(api): add `Selection` type

--- apps/api/src/db/types.ts
+++ apps/api/src/db/types.ts
@@ -1,3 +1,5 @@
+import type { Timestamps } from '@repo/contracts/shared';
+import type { RelationsFieldFilter } from 'drizzle-orm';
 import type { db } from './client.ts';
 
 type Database = typeof db;
@@ -7,3 +9,7 @@ type TransactionHandler = Parameters<Database['transaction']>[0];
 type Transaction = Parameters<TransactionHandler>[0];
 
 export type DatabaseClient = Database | Transaction;
+
+type TimestampFilters = Record<keyof Timestamps, RelationsFieldFilter<Date>>;
+
+export type Selection<Entity> = Partial<Omit<Entity, keyof Timestamps> & TimestampFilters>;
