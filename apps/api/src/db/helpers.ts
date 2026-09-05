5e24a41fd7b41b569e745bdd1ed0c381dd303e17
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-05 12:08:27 -0300

    refactor(api)!: expand `RepositoryError`

--- apps/api/src/db/helpers.ts
+++ apps/api/src/db/helpers.ts
@@ -1,12 +1,50 @@
-export class RepositoryError extends Error {
-	constructor(operation: 'create' | 'find' | 'update' | 'delete', entity: Lowercase<string>) {
-		super(`Failed to ${operation} ${entity}`);
+type EntityName = Capitalize<string>;
+
+type RepositoryInput = Record<string, unknown>;
+
+class RepositoryError extends Error {
+	static isRepositoryError(value: unknown) {
+		return value instanceof RepositoryError;
+	}
+
+	readonly operation;
+	readonly entity;
+	readonly input;
+
+	constructor(
+		operation: 'insertion' | 'selection' | 'update' | 'deletion',
+		entity: EntityName,
+		input: RepositoryInput,
+	) {
+		super(`Entity ${operation} failed`);
+
+		this.operation = operation;
+		this.entity = entity;
+		this.input = input;
 	}
 }
 
-export class CreationError extends RepositoryError {
-	constructor(entity: Lowercase<string>) {
-		super('create', entity);
+export class InsertionError extends RepositoryError {
+	constructor(entity: EntityName, input: RepositoryInput) {
+		super('insertion', entity, input);
+	}
+}
+
+export class SelectionError extends RepositoryError {
+	constructor(entity: EntityName, input: RepositoryInput) {
+		super('selection', entity, input);
+	}
+}
+
+export class UpdateError extends RepositoryError {
+	constructor(entity: EntityName, input: RepositoryInput) {
+		super('update', entity, input);
+	}
+}
+
+export class DeletionError extends RepositoryError {
+	constructor(entity: EntityName, input: RepositoryInput) {
+		super('deletion', entity, input);
 	}
 }
 
