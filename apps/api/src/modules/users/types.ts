3bf33f6a8c45703dcf1cdb670bd49da577260a10
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-06 08:12:37 -0300

    feat(api)!: remove outdated user ID types

--- apps/api/src/modules/users/types.ts
+++ apps/api/src/modules/users/types.ts
@@ -1,10 +1,4 @@
 import type { UserQuery } from '@repo/contracts/users';
 import type { QueryParameters } from '#types.ts';
 
-export type UserId = number | string;
-
-export type RawUserParameters = {
-	userId: UserId;
-};
-
 export type UserQueryParameters = QueryParameters<UserQuery>;
