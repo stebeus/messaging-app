78e670f1a0da9c74497aab0e407e4c58b0648a78
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-06 17:06:06 -0300

    fix(contracts): re-add `UpdateMemberBody` DTO schema

--- packages/contracts/src/modules/members/dto.ts
+++ packages/contracts/src/modules/members/dto.ts
@@ -2,8 +2,14 @@ import * as z from 'zod';
 
 import { id } from '#shared/entities.js';
 
+import { MemberUpdate } from './entity.js';
+
 export const MemberParameters = z.object({
 	memberId: id,
 });
 
+export const UpdateMemberBody = MemberUpdate.pick({ role: true });
+
 export type MemberParameters = z.infer<typeof MemberParameters>;
+
+export type UpdateMemberBody = z.infer<typeof UpdateMemberBody>;
