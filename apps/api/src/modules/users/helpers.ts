d8babf44b63d301fcd533d53d6b879de26eedea8
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-06 08:15:25 -0300

    fix(api): optionalize `displayName`

--- apps/api/src/modules/users/helpers.ts
+++ apps/api/src/modules/users/helpers.ts
@@ -2,5 +2,5 @@ import { contains } from '#db/helpers.ts';
 
 export const userRelations = { friendships: true, groups: true } as const;
 
-export const containsDisplayName = (displayName: string) =>
+export const containsDisplayName = (displayName?: string) =>
 	({ displayName: contains(displayName) }) as const;
