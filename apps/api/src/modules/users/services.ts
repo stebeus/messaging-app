f97e966af7e489351697c20edbc792e4903fc28c
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-06 08:20:07 -0300

    feat(api): pass `tx` in the user service

--- apps/api/src/modules/users/services.ts
+++ apps/api/src/modules/users/services.ts
@@ -1,11 +1,12 @@
 import type { UserParameters } from '@repo/contracts/users';
+import type { DatabaseContext } from '#db/types.ts';
 
 import { NotFoundError } from '#utils/errors.ts';
 
 import * as userRepository from './repository.ts';
 
-export const getOne = async ({ userId }: UserParameters) => {
-	const user = await userRepository.findOne({ id: userId });
+export const getOne = async ({ userId, tx }: DatabaseContext<UserParameters>) => {
+	const user = await userRepository.findOne({ id: userId, tx });
 	if (user == null) throw new NotFoundError({ resource: 'User' });
 	return user;
 };
