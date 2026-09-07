ab1188b5fa94d530a864b34c730158115fd179fb
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-07 14:51:13 -0300

    fix(api): correct user relations

--- apps/api/src/modules/users/helpers.ts
+++ apps/api/src/modules/users/helpers.ts
@@ -1,6 +1,11 @@
 import { contains } from '#db/helpers.ts';
 
-export const userRelations = { friendships: true, groups: true } as const;
+export const userRelations = {
+	groups: true,
+	memberships: true,
+	receivedFriendRequests: true,
+	sentFriendRequests: true,
+} as const;
 
 export const containsDisplayName = (displayName?: string) =>
 	({ displayName: contains(displayName) }) as const;
