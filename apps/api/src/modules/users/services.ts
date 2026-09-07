267d41fdc06a89d71f4f6dcf1706122bda644883
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-07 14:30:35 -0300

    refactor(api): simplify user service

--- apps/api/src/modules/users/services.ts
+++ apps/api/src/modules/users/services.ts
@@ -1,12 +1,11 @@
 import type { UserParameters } from '@repo/contracts/users';
-import type { DatabaseContext } from '#db/types.ts';
 
 import { NotFoundError } from '#utils/errors.ts';
 
 import * as userRepository from './repository.ts';
 
-export const getOne = async ({ userId, tx }: DatabaseContext<UserParameters>) => {
-	const user = await userRepository.findOne({ id: userId, tx });
+export const getOne = async ({ userId }: UserParameters) => {
+	const user = await userRepository.findOne({ id: userId });
 	if (user == null) throw new NotFoundError({ resource: 'User' });
 	return user;
 };
