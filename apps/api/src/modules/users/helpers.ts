a4173280cc69b30dd71bd971f2e8b2d6d5755109
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-08 07:08:50 -0300

    refactor(api): hide user friend requests

--- apps/api/src/modules/users/helpers.ts
+++ apps/api/src/modules/users/helpers.ts
@@ -1,11 +1,6 @@
 import { contains } from '#db/helpers.ts';
 
-export const userRelations = {
-	groups: true,
-	memberships: true,
-	receivedFriendRequests: true,
-	sentFriendRequests: true,
-} as const;
+export const userRelations = { groups: true, memberships: true } as const;
 
 export const containsDisplayName = (displayName?: string) =>
 	({ displayName: contains(displayName) }) as const;
