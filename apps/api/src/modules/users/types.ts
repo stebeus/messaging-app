cb2a91b9f8716199000d6f203f34bed97f688998
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-08 11:12:00 -0300

    style(api): put `UserSelection` after `UsersSelection`

--- apps/api/src/modules/users/types.ts
+++ apps/api/src/modules/users/types.ts
@@ -2,6 +2,6 @@ import type { User, UserQuery } from '@repo/contracts/users';
 import type { Selection } from '#db/types.ts';
 import type { QueryParameters } from '#types.ts';
 
-export type UserSelection = Selection<User>;
-
 export type UsersSelection = QueryParameters<UserQuery>;
+
+export type UserSelection = Selection<User>;
