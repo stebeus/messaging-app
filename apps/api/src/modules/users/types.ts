d54e8747a0fb1edb40c6e9b95462b12edb44313f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-09 07:03:48 -0300

    refactor(api): extract duplicated types into `FindUserParameters`

--- apps/api/src/modules/users/types.ts
+++ apps/api/src/modules/users/types.ts
@@ -1,7 +1,9 @@
-import type { User, UserQuery } from '@repo/contracts/users';
+import type { User, UserParameters, UserQuery } from '@repo/contracts/users';
 import type { Selection } from '#db/types.ts';
 import type { QueryParameters } from '#types.ts';
 
 export type UsersSelection = QueryParameters<UserQuery>;
 
 export type UserSelection = Selection<User>;
+
+export type FindUserParameters = UserParameters & UsersSelection;
