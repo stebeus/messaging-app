81d9c816fe3e6ab65f9cb53f8bead527991b428f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-10 06:52:45 -0300

    refactor(api)!: improve users

--- apps/api/src/modules/users/helpers.ts
+++ apps/api/src/modules/users/helpers.ts
@@ -1,6 +1,11 @@
+import type { Id } from '@repo/contracts/shared';
+
 import { contains } from '#db/helpers.ts';
 
 export const userRelations = { groups: true, memberships: true } as const;
 
 export const containsDisplayName = (displayName?: string) =>
 	({ displayName: contains(displayName) }) as const;
+
+export const createUserFilter = (id: Id, displayName?: string) =>
+	({ where: { ...containsDisplayName(displayName), NOT: { id } } }) as const;
--- apps/api/src/modules/users/types.ts
+++ apps/api/src/modules/users/types.ts
@@ -1,9 +1,15 @@
+import type { Query } from '@repo/contracts/shared';
 import type { User, UserParameters, UserQuery } from '@repo/contracts/users';
 import type { Selection } from '#db/types.ts';
-import type { QueryParameters } from '#types.ts';
+import type { QueryParameters, ScopedQueryParameters } from '#types.ts';
 
 export type UsersSelection = QueryParameters<UserQuery>;
 
 export type UserSelection = Selection<User>;
 
-export type FindUserParameters = UserParameters & UsersSelection;
+export type UserScopedQueryParameters<QueryDto = Query> = ScopedQueryParameters<
+	UserParameters,
+	QueryDto
+>;
+
+export type UserSearchParameters = UserScopedQueryParameters<UserQuery>;
