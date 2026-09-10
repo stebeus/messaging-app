76ccca707c6b1bb0dfeab6a093e44df8eaaa688f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-10 08:05:42 -0300

    feat(api): add `MembersSelection` type

--- apps/api/src/modules/members/types.ts
+++ apps/api/src/modules/members/types.ts
@@ -1,12 +1,16 @@
+import type { ConversationParameters } from '@repo/contracts/conversations';
 import type { GroupParameters } from '@repo/contracts/groups';
 import type { Member, roles } from '@repo/contracts/members';
 import type { Id } from '@repo/contracts/shared';
-import type { UserParameters } from '@repo/contracts/users';
+import type { UserParameters, UserQuery } from '@repo/contracts/users';
 import type { Selection } from '#db/types.ts';
+import type { ScopedQueryParameters } from '#types.ts';
 
 export type MemberParameters = Pick<Member, 'userId' | 'conversationId'>;
 
 export type GroupMemberParameters = UserParameters & GroupParameters;
+
+export type MembersSelection = ScopedQueryParameters<ConversationParameters, UserQuery>;
 
 export type MemberSelection = Selection<Member>;
 
