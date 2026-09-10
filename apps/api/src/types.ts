586d8d33d49899c0ef635f8d34d010df699fc97c
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-10 06:52:12 -0300

    feat(api): add `ScopedQueryParameters` type

--- apps/api/src/types.ts
+++ apps/api/src/types.ts
@@ -3,3 +3,6 @@ import type { Query } from '@repo/contracts/shared';
 export type QueryParameters<QueryDto = Query> = {
 	query: QueryDto;
 };
+
+export type ScopedQueryParameters<Parameters, QueryDto = Query> = Parameters &
+	QueryParameters<QueryDto>;
