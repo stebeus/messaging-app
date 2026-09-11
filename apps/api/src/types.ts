1fca2c835910eb3cc22f4594c683a83b246ac7bf
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-11 10:03:35 -0300

    style(api): change suffix parameters to args

--- apps/api/src/types.ts
+++ apps/api/src/types.ts
@@ -1,8 +1,7 @@
 import type { Query } from '@repo/contracts/shared';
 
-export type QueryParameters<QueryDto = Query> = {
+export type QueryArgs<QueryDto = Query> = {
 	query: QueryDto;
 };
 
-export type ScopedQueryParameters<Parameters, QueryDto = Query> = Parameters &
-	QueryParameters<QueryDto>;
+export type ScopedQueryParameters<Parameters, QueryDto = Query> = Parameters & QueryArgs<QueryDto>;
