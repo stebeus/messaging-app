63e9dcd8daefaeffbb8e15073134744562a96479
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-11 10:28:40 -0300

    refactor(contracts): extract role types from API

--- packages/contracts/src/modules/members/entity.ts
+++ packages/contracts/src/modules/members/entity.ts
@@ -5,12 +5,15 @@ import { id, Timestamps, timestamps } from '#shared/entities.js';
 export const roles = ['member', 'admin', 'owner'] as const;
 
 const role = z.enum(roles);
+const manageableRole = role.exclude(['owner']);
+
+const defaultRole = 'member';
 
 export const Member = z.object({
 	...Timestamps.shape,
 	userId: id,
 	conversationId: id,
-	role: role.default('member'),
+	role: role.default(defaultRole),
 });
 
 export const NewMember = Member.omit(timestamps);
@@ -18,7 +21,7 @@ export const NewMember = Member.omit(timestamps);
 export const MemberUpdate = z
 	.object({
 		...Member.shape,
-		role: role.exclude(['owner']).default('member').optional(),
+		role: manageableRole.default(defaultRole),
 	})
 	.omit(timestamps);
 
@@ -26,4 +29,10 @@ export type Member = z.infer<typeof Member>;
 
 export type NewMember = z.input<typeof NewMember>;
 
-export type MemberUpdate = z.infer<typeof MemberUpdate>;
+export type MemberUpdate = z.input<typeof MemberUpdate>;
+
+export type Role = z.infer<typeof role>;
+
+export type Roles = typeof roles;
+
+export type ManageableRole = z.infer<typeof manageableRole>;
