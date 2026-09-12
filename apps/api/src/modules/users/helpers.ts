cb39700f6798fe16442a9f67edc14a8fac62eac1
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-12 09:11:16 -0300

    fix(api): return nothing for empty name queries

--- apps/api/src/modules/users/helpers.ts
+++ apps/api/src/modules/users/helpers.ts
@@ -4,8 +4,9 @@ import { contains } from '#db/helpers.ts';
 
 export const userRelations = { groups: true, memberships: true } as const;
 
-export const containsName = (name?: string) => ({
-	OR: [{ username: contains(name) }, { displayName: contains(name) }],
-});
+export const containsName = (name?: string) =>
+	name == null
+		? undefined
+		: { OR: [{ username: contains(name) }, { displayName: contains(name) }] };
 
 export const createUserFilter = (id: Id) => ({ where: { NOT: { id } } }) as const;
