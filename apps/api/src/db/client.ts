ee758817900195c73b2b0533347cde7e77ada62c
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-13 12:20:54 -0300

    chore(api): remove test drizzle setup remanents

--- apps/api/src/db/client.ts
+++ apps/api/src/db/client.ts
@@ -4,14 +4,7 @@ import { env } from '#env.ts';
 
 import { conversationRelations, relationshipRelations, userRelations } from './relations/index.ts';
 
-const relations = { ...conversationRelations, ...relationshipRelations, ...userRelations };
-
 export const db = drizzle({
 	connection: env.DATABASE_URL,
-	relations,
-});
-
-export const testDb = drizzle({
-	connection: env.TEST_DATABASE_URL,
-	relations,
+	relations: { ...conversationRelations, ...relationshipRelations, ...userRelations },
 });
