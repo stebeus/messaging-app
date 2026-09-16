2f0d3b026475f42d08377a05a9df4a75a02e1af8
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-16 12:39:09 -0300

    feat(contracts): add member response DTos

--- packages/contracts/src/modules/members/dto.ts
+++ packages/contracts/src/modules/members/dto.ts
@@ -1,9 +1,10 @@
 import * as z from 'zod';
 
 import { GroupParams } from '#modules/groups/dto.js';
+import { User } from '#modules/users/entity.js';
 import { id } from '#shared/entities.js';
 
-import { MemberUpdate } from './entity.js';
+import { Member, MemberUpdate } from './entity.js';
 
 export const MemberParams = z.object({
 	...GroupParams.shape,
@@ -12,6 +13,17 @@ export const MemberParams = z.object({
 
 export const UpdateMemberBodyRequest = MemberUpdate.pick({ role: true });
 
+export const MemberResponse = z.object({
+	...Member.shape,
+	user: User,
+});
+
+export const ListMemberResponse = z.array(MemberResponse);
+
 export type MemberParams = z.infer<typeof MemberParams>;
 
 export type UpdateMemberBodyRequest = z.infer<typeof UpdateMemberBodyRequest>;
+
+export type MemberResponse = z.infer<typeof MemberResponse>;
+
+export type ListMemberResponse = z.infer<typeof ListMemberResponse>;
