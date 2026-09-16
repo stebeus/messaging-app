4247ea3dc21db772f7468d3ae08f4e840ed9892c
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-16 12:39:20 -0300

    feat(contracts): add group response DTOs

--- packages/contracts/src/modules/groups/dto.ts
+++ packages/contracts/src/modules/groups/dto.ts
@@ -1,9 +1,11 @@
 import * as z from 'zod';
 
+import { Conversation } from '#modules/conversations/entity.js';
 import { MessageParams } from '#modules/messages/dto.js';
+import { User } from '#modules/users/entity.js';
 import { id } from '#shared/entities.js';
 
-import { GroupUpdate, NewGroup } from './entity.js';
+import { Group, GroupUpdate, NewGroup } from './entity.js';
 
 export const GroupParams = z.object({
 	groupId: id,
@@ -18,6 +20,14 @@ export const CreateGroupBodyRequest = NewGroup.omit({ conversationId: true, owne
 
 export const UpdateGroupBodyRequest = GroupUpdate.omit({ conversationId: true });
 
+export const GroupResponse = z.object({
+	...Group.shape,
+	conversation: Conversation,
+	owner: User,
+});
+
+export const ListGroupResponse = z.array(GroupResponse);
+
 export type GroupParams = z.infer<typeof GroupParams>;
 
 export type GroupMessageParams = z.infer<typeof GroupMessageParams>;
@@ -25,3 +35,7 @@ export type GroupMessageParams = z.infer<typeof GroupMessageParams>;
 export type CreateGroupBodyRequest = z.input<typeof CreateGroupBodyRequest>;
 
 export type UpdateGroupBodyRequest = z.infer<typeof UpdateGroupBodyRequest>;
+
+export type GroupResponse = z.infer<typeof GroupResponse>;
+
+export type ListGroupResponse = z.infer<typeof ListGroupResponse>;
