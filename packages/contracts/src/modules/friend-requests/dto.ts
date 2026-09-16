e88a8ba4cdf3cc1b74b34a673d55dec7e601e019
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-16 12:39:38 -0300

    feat(contracts): add friend request response DTOs

--- packages/contracts/src/modules/friend-requests/dto.ts
+++ packages/contracts/src/modules/friend-requests/dto.ts
@@ -1,6 +1,9 @@
 import * as z from 'zod';
 
+import { User } from '#modules/users/entity.js';
 import { id } from '#shared/entities.js';
+
+import { FriendRequest } from './entity.js';
 
 export const FriendRequestParams = z.object({
 	recipientId: id,
@@ -10,6 +13,20 @@ export const AcceptFriendRequestParams = z.object({
 	requesterId: id,
 });
 
+export const FriendRequestResponse = z.object({
+	...FriendRequest.shape,
+	user: User,
+});
+
+export const ListFriendRequestResponse = z.object({
+	sent: z.array(FriendRequestResponse),
+	received: z.array(FriendRequestResponse),
+});
+
 export type FriendRequestParams = z.infer<typeof FriendRequestParams>;
 
 export type AcceptFriendRequestParams = z.infer<typeof AcceptFriendRequestParams>;
+
+export type FriendRequestResponse = z.infer<typeof FriendRequestResponse>;
+
+export type ListFriendRequestResponse = z.infer<typeof ListFriendRequestResponse>;
