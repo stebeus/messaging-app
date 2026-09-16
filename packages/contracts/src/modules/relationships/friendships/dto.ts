0c639faf8fc55e9f53d02363690280948a76d5cf
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-16 12:38:37 -0300

    feat(contracts): add friendship response DTOs

--- packages/contracts/src/modules/relationships/friendships/dto.ts
+++ packages/contracts/src/modules/relationships/friendships/dto.ts
@@ -1,9 +1,23 @@
 import * as z from 'zod';
 
+import { User } from '#modules/users/entity.js';
 import { id } from '#shared/entities.js';
+
+import { Friendship } from './entity.js';
 
 export const FriendParams = z.object({
 	friendId: id,
 });
 
+export const FriendResponse = z.object({
+	...Friendship.shape,
+	friend: User,
+});
+
+export const ListFriendResponse = z.array(FriendResponse);
+
 export type FriendParams = z.infer<typeof FriendParams>;
+
+export type FriendResponse = z.infer<typeof FriendResponse>;
+
+export type ListFriendResponse = z.infer<typeof ListFriendResponse>;
