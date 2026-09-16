4f844f9c75c8c3497bc30f663ca0fd7f9311d22c
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-16 14:27:48 -0300

    feat(web): add `CommandProps` type

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -4,6 +4,8 @@ import { createSafeContext } from '#hooks/context.tsx';
 
 export type ButtonProps = ComponentPropsWithoutRef<'button'>;
 
+export type CommandProps = Omit<ButtonProps, 'command'>;
+
 export type SubmitButtonProps = Omit<ButtonProps, 'type'>;
 
 export const Button = ({ type = 'button', ...props }: ButtonProps) => (
@@ -17,11 +19,11 @@ export const InvokerButton = (props: ButtonProps) => {
 	return <Button {...props} commandFor={id} />;
 };
 
-export const PopoverTrigger = (props: ButtonProps) => (
+export const PopoverTrigger = (props: CommandProps) => (
 	<InvokerButton {...props} command="toggle-popover" />
 );
 
-export const PopoverClose = (props: ButtonProps) => (
+export const PopoverClose = (props: CommandProps) => (
 	<InvokerButton {...props} command="hide-popover" />
 );
 
