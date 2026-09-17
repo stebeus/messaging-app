5197f9d3a4d5724de6780f0857811b8bc50cb457
stebeus <189894010+stebeus@users.noreply.github.com>
2026-09-17 08:01:35 -0300

    chore(web): set up Tanstack form

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,12 +1,20 @@
+import { TanStackDevtools } from '@tanstack/react-devtools';
+import { formDevtoolsPlugin } from '@tanstack/react-form-devtools';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
 
 import '#index.css';
 
 const Root = () => (
 	<>
 		<Outlet />
-		<TanStackRouterDevtools position="bottom-right" />
+		<TanStackDevtools
+			config={{ position: 'bottom-right' }}
+			plugins={[
+				formDevtoolsPlugin(),
+				{ name: 'TanStack Router', render: <TanStackRouterDevtoolsPanel /> },
+			]}
+		/>
 	</>
 );
 
